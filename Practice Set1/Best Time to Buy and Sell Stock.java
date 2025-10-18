2b17ed40ca70d7639b9de4647d077981cfabce41
Himans110 <himanshgoel110@gmail.com>
2025-10-18 23:19:18 +0700

    Best Time To Buy and Sell Stock

---
+++ Practice Set1/Best Time to Buy and Sell Stock.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] arr) {
+        int buy = arr[0];
+        int profit = 0;
+        for(int i = 1 ; i < arr.length ; i++){
+            if(arr[i] > buy){
+                profit = Math.max(profit, arr[i]-buy);
+            }
+            buy = Math.min(buy , arr[i]);
+        }
+        return profit;
+    }
+}
